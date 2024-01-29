3bc16ce9ec4b006019f0c63110bf6008b93ae06e
Larc Oliveira <146396747+cclar13@users.noreply.github.com>
2024-01-29 01:51:17 -0300

    index com novas atulizações

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       <nav class="menu-desktop">
         <ul>
           <li><a href="">Home</a></li>
-          <li><a href="">Especialidades</a></li>
+          <li><a href="">Habilidades</a></li>
           <li><a href="">Sobre</a></li>
           <li><a href="">Projetos</a></li>
         </ul>
@@ -35,6 +35,50 @@
       </div><!-- btn-contato: -->
     </div> <!-- configuracao  -->
   </header>
+  <main>
+    <section class="topo-do-site">
+      <div class="configuracao">
+        <div class="flex">
+          <div class="text-topo-do-site">
+            <h1>TRABALHANDO COM GIT E GITHUB<span>.</span></h1>
+            <p>Trabalho final com git e github , mostrando um pouco do que é possivil com o curso de desenvolvimento de sistemas com a instituicao senai , na cidade de governador valadares.</p>
+            <div class="btn-contato">
+              <a href="">
+                <button>Entre em contato</button>
+              </a>
+            </div><!--btn-contato-->
+          </div><!--text topo-site-->
+          <div class="img-topo-do-site">
+            <img src="./img/contatoicon.png" alt="icon de contato">
+          </div><!--img-topo-site-->
+        </div> <!--flex-->
+      </div> <!--configuracao-->
+    </section> <!--topo-do-site-->
+    <section class="habilidades">
+      <div class="configuracao">
+        <h2 class="titulo"><b>MINHAS <span>HABILIDADES.</span></b></h2>
+        <div class="flex">
+          <div class="habilidades-box">
+            <i class="bi bi-code-square"></i>
+            <h3>Ferramentas</h3>
+            <p>Ferramentas como html,js,css,php e mysql.Cria-se um site cheio de interação com o cliente e designer atrativo</p>
+          </div><!--habilidades-box-->
+
+          <div class="habilidades-box">
+            <i class="bi bi-chat"></i>
+            <h3>Comunicação </h3>
+            <p>Habilidade de comunicação se equivale na organição de equipe,onde é usada para a criação de projeto em equipe.</p>
+          </div><!--habilidades-box-->
+
+          <div class="habilidades-box">
+            <i class="bi bi-search"></i>
+            <h3>Persistencia</h3>
+            <p>Programação nunca para,por isso a habilidade de persistencia na busca do conhecimento sempre vai ser algo contante</p>
+          </div><!--habilidades-box-->
+        </div><!--flex-->
+      </div> <!--configuracao-->
+    </section> <!--habilidades-->
+  </main>
   <script src="./js/js.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
